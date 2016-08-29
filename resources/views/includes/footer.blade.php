e1939c08d0f73a17fda7efd3a9423199414e2a27
Willchalfan <f.waicung@gmail.com>
2016-08-29 13:11:01 +1000

    home page

---
+++ resources/views/includes/footer.blade.php
@@ -0,0 +1,7 @@
+<!-- Footer -->
+<footer class="container-fluid bg-4 text-center">
+  <p>The Wayfinding Experiment</p>
+</footer>
+
+</body>
+</html>
---
+++ resources/views/index.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts/no-side-bar')
+
+@section('content')
+
+<!-- First Container -->
+<div class="container-fluid bg-1 text-center">
+  <h3 class="margin">Want to be the volunteer?</h3>
+  <a class="w3-btn-floating-large w3-teal text-center" style="width:240px;height:240px;padding:70px;">JOIN NOW</a>
+  {{-- <img src="bird.jpg" class="img-responsive img-circle margin" style="display:inline" alt="Bird" width="350" height="350"> --}}
+  <h3>I'm an adventurer</h3>
+</div>
+
+<!-- Second Container -->
+{{-- <div class="container-fluid bg-2 text-center">
+  <h3 class="margin">What is this?</h3>
+  <p>A Wayfinding Experiment</p>
+  <a href="#" class="btn btn-default btn-lg">
+    <span class="glyphicon glyphicon-search"></span> Search
+  </a>
+</div> --}}
+
+
+
+@endsection
